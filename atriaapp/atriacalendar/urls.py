6de7133dc924ec498d38749570d26da3176bd61d
Ian Costanzo <ian@anon-solutions.ca>
2018-10-22 15:50:16 -0700

    Add swingtime views to Atria calendar

--- atriaapp/atriacalendar/urls.py
+++ atriaapp/atriacalendar/urls.py
@@ -3,6 +3,7 @@ URL configuration for Atria Calendar app.
 """
 
 from django.urls import path
+from django.conf.urls import url, include
 
 from .views import *
 
@@ -11,11 +12,12 @@ urlpatterns = [
     path('login/', login, name='login'),
 
     path('', calendar_home, name='calendar_home'),
-    path('calendar/', calendar_view, name='calendar_view'),
+    path('calendar/<int:year>/<int:month>/', calendar_view, name='calendar_view'),
 
     path('create-event/', create_event, name='create_event'),
     path('create-event/participants/', add_participants,
          name='add_participants'),
     path('event-list/', event_list, name='event_list'),
     path('event-detail/', event_detail, name='event_detail'),
+    url(r'^swingtime/', include('swingtime.urls')),
 ]
