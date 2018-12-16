b3a0a06c3d539a49716c3e1b9f1e740626f3cf67
Ian Costanzo <ian@anon-solutions.ca>
2018-12-16 09:30:34 +0100

    Update release commands

--- atriaapp/release_tasks.sh
+++ atriaapp/release_tasks.sh
@@ -1,4 +1,4 @@
-python manage.py makemigrations
+python manage.py makemigrations atriacalendar
 python manage.py migrate
 python manage.py createsuperuser --username admin@email.com --email admin@email.com
 python manage.py loaddata sample-data
