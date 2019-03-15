82debc3401b23920f62c3caf45d592c10e0c82bb
Ian Costanzo <ian@anon-solutions.ca>
2019-03-15 09:42:49 -0700

    Remove vue test views

--- atriaapp/atriacalendar/views.py
+++ atriaapp/atriacalendar/views.py
@@ -375,15 +375,3 @@ def search_neighbour_view(request):
 
 def search_organization_view(request):
     return render(request, 'atriacalendar/pagesSearch/organizationsSearch.html')
-
-def handle_vue_demo(request):
-    return render(request, 'atriacalendar/vue_demo.html')
-
-def handle_vue_demo2(request):
-    return render(request, 'atriacalendar/vue_demo2.html')
-
-def handle_vue_demo3(request):
-    return render(request, 'atriacalendar/vue_demo3.html')
-
-def handle_vue_demo4(request):
-    return render(request, 'atriacalendar/vue_demo4.html')
