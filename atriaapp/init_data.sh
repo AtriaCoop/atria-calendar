b25105d4e09512ff30cb766a10139b7a2e89fb4c
Ian Costanzo <ian@anon-solutions.ca>
2019-04-29 08:07:25 -0700

    Added test data and config for indy networks
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- atriaapp/init_data.sh
+++ atriaapp/init_data.sh
@@ -1,6 +1,3 @@
 python manage.py loads_orgs ./trustee-org.yml
-sleep 5
-python manage.py loads_schemas ./myco-schemas.yml 1
-sleep 5
-python manage.py loads_orgs ./myco-orgs.yml
-sleep 5
+python manage.py loads_schemas ./test-schemas.yml 1
+python manage.py loads_orgs ./test-orgs.yml
